06ab69f2274cf5c69935a65147efbb9fe0e0715c
bhavyalatha26 <cuty2689@gmail.com>
2015-10-24 22:28:09 +0530

    Update User.scala

--- app/model/User.scala
+++ app/model/User.scala
@@ -3,10 +3,6 @@ package model
 import play.api.data.Form
 import play.api.data.Forms._
 
-/**
- * Created by Bhavya on 14-10-2015.
- **/
-
 case class User(id : Long,firstName : String, lastName : String , mobile : Long , email : String)
 
 case class UserFormData(firstName : String, lastName : String , mobile : Long , email : String )
